00dd76f12668224d3447e7fc0883448e4b1b66b6
AmaniceGuy <51844045+AmaniceGuy@users.noreply.github.com>
2020-08-30 13:24:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 import classes2 as classes
 import ui
 pygame.init()
@@ -27,11 +28,11 @@ HARDEST_GRID = [[8, 0, 0, 0, 0, 0, 0, 0, 0],
                 [0, 9, 0, 0, 0, 0, 4, 0, 0]]
 
 # global screen constant for everything to be displayed on
-SCREEN = pygame.display.set_mode((1000, 500))
+SCREEN = pygame.display.set_mode((700, 500))
 pygame.display.set_caption("Small Brain Sudoku Go Brrrrrrrrrrrr")
 
 # Black background for the screen
-BACKGROUND = pygame.Surface((1000, 500))
+BACKGROUND = pygame.Surface((700, 500))
 BACKGROUND.fill((0, 0, 0))
 
 # Blit background onto the screen
@@ -42,6 +43,11 @@ CLOCK = pygame.time.Clock()
 
 # Global bool constant for whether the user wants the program to run
 RUN = True
+
+EVERY_CORD = []
+for x in range(9):
+    for y in range(9):
+        EVERY_CORD.append((x, y))
 
 
 def solve(grid):
@@ -71,6 +77,8 @@ def solve(grid):
                 if event.key == pygame.K_ESCAPE:
                     RUN = False
 
+        if len(grids) == 0:
+            return False
         # solving algorithm
         current_grid = grids[-1]            # choose the last grid in the list of grids
         result = current_grid.solve()       # do a single step in the solving process
@@ -85,7 +93,7 @@ def solve(grid):
             grids.extend(result)                # replace it with the different possibilities
 
         # refresh the screen and board
-        draw_board(SCREEN, None, (-1, -1))
+        draw_board(SCREEN, None, [-1, -1])
         current_grid.draw(SCREEN, is_solve=True)
         pygame.display.flip()
     # return the solved grid if it is solved
@@ -95,12 +103,12 @@ def solve(grid):
 def main():
     """The main function of the program."""
     # initialize a Grid object that the user can interact with
-    grid = classes.Grid([[0] * 9] * 9, show_wrong=False, show_possible=False)
+    grid = classes.Grid([[0] * 9] * 9, show_conflicts=False, show_possible=False)
 
     # surface object that indicates the square that the user has selected
     selected_square = pygame.Surface((50, 50))
     selected_square.fill((120, 120, 120))
-    selected_cord = (-1, -1)
+    selected_cord = [-1, -1]
 
     # key constants for number input
     number_key = {pygame.K_1: 1, pygame.K_2: 2, pygame.K_3: 3, pygame.K_4: 4, pygame.K_5: 5,
@@ -114,8 +122,10 @@ def main():
     check_button = ui.Button("Check", (525, 100))
     show_button = ui.Button("Show Possible", (525, 175))
     clear_button = ui.Button("Clear", (525, 250))
+    load_random = ui.Button("Generate Grid", (525, 325))
+    hardest_button = ui.Button("Load Hardest", (525, 400))
     # list of buttons
-    buttons = [solve_button, check_button, clear_button, show_button]
+    buttons = [solve_button, check_button, clear_button, show_button, load_random, hardest_button]
 
     global RUN
 
@@ -129,20 +139,30 @@ def main():
             elif event.type == pygame.KEYDOWN:
                 # keyboard input - inserting and deleting numbers on the board
                 if event.key == pygame.K_ESCAPE:
-                    selected_cord = (-1, -1)                    # deselect the square
+                    selected_cord = [-1, -1]                    # deselect the square
                 elif event.key in [pygame.K_DELETE, pygame.K_BACKSPACE]:
                     grid.set_square(selected_cord, 0)           # clear the selected square
                 elif event.key in number_key.keys():
                     number = number_key[event.key]
-                    grid.set_square(selected_cord, number)      # place a number in a square
-                elif event.key == pygame.K_F12:         # F12 loads the "hardest" grid
-                    grid = classes.Grid(HARDEST_GRID)
+                    grid.set_square(selected_cord, number, override_can_have=True)      # place a number in a square
+                elif event.key == pygame.K_UP:
+                    if selected_cord[0] != 0:
+                        selected_cord[0] -= 1
+                elif event.key == pygame.K_DOWN:
+                    if selected_cord[0] != 8:
+                        selected_cord[0] += 1
+                elif event.key == pygame.K_LEFT:
+                    if selected_cord[1] != 0:
+                        selected_cord[1] -= 1
+                elif event.key == pygame.K_RIGHT:
+                    if selected_cord[1] != 8:
+                        selected_cord[1] += 1
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 # Mouse input - selecting a square on the grid and buttons
                 pos = pygame.mouse.get_pos()
                 x, y = pos
                 if x in range(25, 475) and y in range(25, 475):
-                    selected_cord = ((y - 25) // 50, (x - 25) // 50)
+                    selected_cord = [(y - 25) // 50, (x - 25) // 50]
                 elif x in range(500, 650):
                     if y in range(25, 75):
                         solved_grid = solve(grid)
@@ -153,12 +173,16 @@ def main():
                             solve_button.cannot_solve()
                     elif y in range(100, 150):
                         grid.check()
-                        grid.check_correct()
+                        grid.show_conflicts = not grid.check_correct()
                     elif y in range(175, 225):
                         grid.assign_possible()
                         grid.show_possible = not grid.show_possible
                     elif y in range(250, 300):
                         grid.clear()
+                    elif y in range(325, 375):
+                        grid = generate_grid()
+                    elif y in range(400, 450):
+                        grid = classes.Grid(HARDEST_GRID, show_possible=False)
 
         # refresh the screen, board, and buttons
         draw_board(SCREEN, selected_square, selected_cord)
@@ -169,11 +193,38 @@ def main():
         pygame.display.flip()
 
 
+def generate_grid():
+    """This function generates a random grid of a random difficulty and returns
+        it as a Grid object. It creates an empty grid, fills a random square,
+        the solves it. Then it randomly removes"""
+    grid = classes.Grid([[0] * 9] * 9)
+    x, y = random.randrange(9), random.randrange(9)
+    grid.set_square((x, y), random.randrange(1, 10))
+    cords = EVERY_CORD.copy()
+
+    grid = solve(grid)
+    grid.show_possible = False
+
+    squares_to_clear = []
+
+    for i in range(50 + random.randrange(0, 19)):
+        cord_index = random.randrange(len(cords))
+        cord = cords[cord_index]
+        cords.pop(cord_index)
+        squares_to_clear.append(cord)
+
+    for cord in squares_to_clear:
+        grid.set_square(cord, 0)
+    grid.set_board()
+
+    return grid
+
+
 def draw_board(screen, selected_square, cord):
     """This function draws a 9 x 9 grid and the selected square indicator
         onto the passed surface"""
     screen.fill((0, 0, 0))
-    if cord != (-1, -1):
+    if cord != [-1, -1]:
         screen.blit(selected_square, (cord[1] * 50 + 25, cord[0] * 50 + 25))
 
     for i in range(10):
